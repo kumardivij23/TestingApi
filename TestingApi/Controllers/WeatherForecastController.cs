7b0778011155c0a779ec5902acbfea5ebff23162
Kumar Divij <75716308+kumardivij23@users.noreply.github.com>
2026-03-09 22:11:24 +0530

    Add Console.WriteLine to WeatherForecastController - AP-51

--- TestingApi/Controllers/WeatherForecastController.cs
+++ TestingApi/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace TestingApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine("hello");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
