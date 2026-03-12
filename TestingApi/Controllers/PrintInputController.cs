56b3c89570887bed7d0a21ee95921e5816b0d394
Kumar Divij <75716308+kumardivij23@users.noreply.github.com>
2026-03-12 13:10:06 +0530

    Add PrintInputController - returns welcome message when called

---
+++ TestingApi/Controllers/PrintInputController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrintInputController : ControllerBase
+    {
+        private readonly ILogger<PrintInputController> _logger;
+
+        public PrintInputController(ILogger<PrintInputController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello welcome to the controller print";
+        }
+    }
+}
