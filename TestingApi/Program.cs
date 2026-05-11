a01dd64312cf807b2c6f646c9c5b16a93845578c
RutujaP-02 <rgpansare@gmail.com>
2026-05-11 20:17:12 +0530

    feat(config): register AppDbContext and SkillService in DI container

--- TestingApi/Program.cs
+++ TestingApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using TestingApi.Data;
+using TestingApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register EF Core InMemory database context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("CcogSkillsDb"));
+
+// Register application services
+builder.Services.AddScoped<ISkillService, SkillService>();
 
 var app = builder.Build();
 
